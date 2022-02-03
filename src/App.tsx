8344f5215e5402fd498b501ca0efa2b8ee37029b
Pedro Rodrigues <pedro.roguea@gmail.com>
2022-02-03 02:25:34 -0300

    improve styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { ButtonDefault } from "./components/Button";
 import { GlobalStyle } from "./styles/global";
-import { Container, Header, Image, Logo, Text } from "./styles/home";
+import { Container, Header, Highlight, Image, Logo, Main, Paragraph, Text, Title } from "./styles/home";
 
 import keyboardImg from "./assets/keyboard.jpg";
+import keyboard2Img from "./assets/keyboard-2.jpg";
+import keyboard3Img from "./assets/keyboard-3.jpg";
 
 function App() {
 
@@ -11,19 +13,33 @@ function App() {
       <Container>
         <Header>
           <Logo>
-            <span>TM</span>
+            TM
           </Logo>
           <ButtonDefault text={"pre-order now"} />
         </Header>
-        <main>
-          <section className="highlight">
-            <h1 className="title">typemaster keyboard</h1>
-            <Text>Improve your productivity and gaming without breaking the bank. Upgrade to a high quality mechanical typing experience.</Text>
+        <Main>
+          <Highlight>
+            <Title>typemaster keyboard</Title>
+            <Paragraph>Improve your productivity and gaming without breaking the bank. Upgrade to a high quality mechanical typing experience.</Paragraph>
             <ButtonDefault hightlightButton text={"pre-order now"} />
-            <span className="release">release on 8/11</span>
-          </section>
-          <Image imageSource={keyboardImg} />
-        </main>
+            <Text className="release">release on 8/11</Text>
+          </Highlight>
+          <Image 
+            imageSource={keyboardImg}
+            sizeWidth="100vw"
+            sizeHeight="340px" 
+          />
+          <Image 
+            imageSource={keyboard2Img}
+            sizeWidth="100px"
+            sizeHeight="150px" 
+          />
+          <Image 
+            imageSource={keyboard3Img}
+            sizeWidth="100px"
+            sizeHeight="150px" 
+          />
+        </Main>
       </Container>
       
       <GlobalStyle />
--- src/styles/home.ts
+++ src/styles/home.ts
@@ -3,6 +3,8 @@ import styled from "styled-components";
 
 interface ImageProps {
   imageSource: string;
+  sizeWidth: string;
+  sizeHeight: string;
 }
 
 export const Container = styled.div`
@@ -10,41 +12,15 @@ export const Container = styled.div`
   display: grid;
   overflow-x: hidden;
   width: 100vw;
-
-  main {
-    width: 100%;
-
-    .highlight {
-      padding: 1rem;
-      display: grid;
-      grid-template-columns: 1fr 1fr;
-      gap: 32px;
-      position: relative;
-
-      .title {
-        grid-column: span 2;
-        font-weight: 700;
-        text-transform: uppercase;
-        font-size: 48px;
-        color: var(--text-title);
-      }
-
-      .release {
-        color: var(--text-body);
-        text-transform: uppercase;
-        font-weight: 600;
-        place-self: center;
-      }
-    }
-  }
 `;
 
 export const Header = styled.header`
-  padding: 1rem;
+  padding: 0 1.5rem;
+  padding-top: 1.5rem;
   display: grid;
   grid-template-columns: 1fr 1fr;
   align-items: center;
-  margin-bottom: 56px;
+  margin-bottom: 64px;
 `;
 
 export const Logo = styled.div`
@@ -56,7 +32,39 @@ export const Logo = styled.div`
   padding: 4px;
 `;
 
-export const Text = styled.p`
+export const Main = styled.main`
+  width: 100%;
+  display: grid;
+`;
+
+export const Highlight = styled.section`
+  padding: 0 1.5rem;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 32px;
+  position: relative;
+  margin-bottom: 64px;
+`;
+
+export const Title = styled.h1`
+  grid-column: span 2;
+  font-weight: 900;
+  text-transform: uppercase;
+  font-size: 48px;
+  color: var(--text-title);
+`;
+
+export const Text = styled.span`
+  color: var(--text-body);
+  place-self: center;
+
+  &.release {
+    text-transform: uppercase;
+    font-weight: 600;
+  }
+`;
+
+export const Paragraph = styled.p`
   grid-column: span 2;
   color: var(--text-body);
   line-height: 24px;
@@ -64,12 +72,12 @@ export const Text = styled.p`
 
 export const Image = styled.div<ImageProps>`
  border-radius: 24px; 
- width: 100vw;
- height: 340px;
+ width: ${props => props.sizeWidth};
+ height: ${props => props.sizeHeight};
  background-image: url(${props => props.imageSource});
- background-position: 25% 70%;
+ background-position: 26% 60%;
  background-repeat:no-repeat; 
  background-size: auto 120%;
  position: relative;
- left: 1rem;
+ right: -1.5rem;
 `;
